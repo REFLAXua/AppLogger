7ca59bf6ae23d4138fc5eba04620364a84e2e6a8
REFLAXua <114022873+REFLAXua@users.noreply.github.com>
2025-01-20 17:59:14 +0200

    add LogLevel

---
+++ AppLogger/LogLelves.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logger
+{
+    public enum LogLevels
+    {
+        Trace,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Critical
+    }
+}
+
